c7a5f3813aad579ee150c533fb0d2fa2dc4205b8
Developer <dev@MBP13Ci5.fritz.box>
2018-05-23 22:16:41 +0200

    tutorial finnished

--- src/index.js
+++ src/index.js
@@ -25,7 +25,7 @@ class Game extends React.Component {
 
 	render() {
 		const history = this.state.history;
-		const current = history[history.length - 1];
+		const current = history[this.state.stepNumber];
 		const chichi = calculateChichi(current.squares);
 
 		const moves = history.map((step, move) => {
@@ -65,7 +65,7 @@ class Game extends React.Component {
 	}
 
 	handleClick(i) {
-		const history = this.state.history;
+		const history = this.state.history.slice(0,this.state.stepNumber + 1);
 		const current = history[history.length - 1];
 		const squares = current.squares.slice();
 		if (calculateChichi(squares) || squares[i]) {
@@ -76,6 +76,7 @@ class Game extends React.Component {
 			history: history.concat([{
 				squares: squares
 			}]),
+			stepNumber: history.length,
 			xIsNext: !this.state.xIsNext
 		});
 	}
